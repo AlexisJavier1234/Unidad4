d10a16bb6baf26cb2fedebe7939d916c5d49a206
AlexisJavier1234 <143725720+AlexisJavier1234@users.noreply.github.com>
2023-11-07 21:14:32 -0600

    TareaArreglos
    TareaArreglas

---
+++ ArreglosBidimencionales.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arreglosbidimencionales;
+
+/**
+ *
+ * @author USER
+ */
+public class ArreglosBidimencionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char[][] opciones = 
+        {
+            {'a', 'b', 'c', 'd', 'e'},
+            {'f', 'g', 'h', 'i', 'j'},
+            {'k', 'l', 'm', 'n', 'o'}
+        };
+        for (int i = 0; i <3; i++) {
+        for (int j = 0; j < 5; j++) 
+            
+            {
+                
+            System.out.print(opciones [i][j] + " ");
+            }
+            System.out.println();
+}
+}
+}
+
+
